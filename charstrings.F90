e3d84d958be6288cfa1ee6810fc1a8313525511f
denise.worthen <Denise.Worthen@noaa.gov>
2019-12-03 17:44:36 +0000

    fixed mask file and variable name for half-deg

--- charstrings.F90
+++ charstrings.F90
@@ -7,18 +7,19 @@ module charstrings
     '/scratch1/NCEPDEV/nems/emc.nemspara/RT/FV3-MOM6-CICE5/benchmark-20191112/MOM6_FIX_025deg/'
 !    '/scratch1/NCEPDEV/nems/emc.nemspara/RT/FV3-MOM6-CICE5/benchmark-20180913/MOM6_FIX_025deg/'
   character(len= 10) :: res = 'mx025'
+
+  character(len=100) :: maskfile = 'ocean_topog.nc'
+  character(len= 12) :: maskname = 'wet'
 #endif
 #ifdef output_grid_hdeg
   character(len=256) :: dirsrc = &
-    '/scratch2/NCEPDEV/stmp1/Jessica.Meixner/ForCristiana/halfdegbaseline/MOM6_FIX_05deg/'    
+    '/scratch1/NCEPDEV/nems/emc.nemspara/RT/FV3-MOM6-CICE5/master-20191125/MOM6_FIX/050/'
   character(len= 10) :: res = 'mx050'
+
+  character(len=100) :: maskfile = 'ocean_mask.nc'
+  character(len= 12) :: maskname = 'mask'
 #endif
-
-  !character(len=256) :: dirout = '/scratch2/NCEPDEV/climate/Denise.Worthen/TTout/'
-  character(len=256) :: dirout = './'
-  character(len=100) :: maskfile = 'ocean_topog.nc'
-  character(len= 12) :: maskname = 'wet'
-
+  character(len=256) :: dirout = '/scratch2/NCEPDEV/climate/Denise.Worthen/TTout/'
   character(len=256) :: history
   character(len=  8) :: cdate
 
